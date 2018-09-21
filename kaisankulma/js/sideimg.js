7395d89fa337351cea541bd7fb0ff224ecc0f00f
NikoEOSP <43313697+NikoEOSP@users.noreply.github.com>
2018-09-21 18:07:07 +0300

    responsiveness

--- kaisankulma/js/sideimg.js
+++ kaisankulma/js/sideimg.js
@@ -1,3 +1,12 @@
 function scalesi(){
-	$("#siimg").height($("#stext").height()+"px");
+	//$("#sideImg").height(($("#sideText").height())+"px");
+	//$("#sideImg").width(($("#maincontent").width())+"px");
+	
+	$('.sideImg').each(function(){
+		var parentHeight = $(this).parent().height() - 2;
+		var parentWidth = $(this).parent().width();
+		$(this).height(parentHeight);
+		$(this).width(parentWidth);   	  
+});
 }
+
